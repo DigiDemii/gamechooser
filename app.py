b49fcc269a6f6fbfae98ce7aae96f623562bd6d5
Demi Taylor <80684537+DigiDemii@users.noreply.github.com>
2022-02-01 12:13:05 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ from flask import Flask, Blueprint, render_template, url_for, redirect
 from model import db
 import random
 
-# views = Blueprint(__name__, "views")
+views = Blueprint(__name__, "views")
 
 app = Flask(__name__)
 
-# app.register_blueprint(views, url_prefix="/")
+app.register_blueprint(views, url_prefix="/")
 
 
-@app.route("/index")
+@app.route("/home")
 def homepage():
     card = random.choice(db)
     return render_template("index.html", card=card)
