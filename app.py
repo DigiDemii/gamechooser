bd77e1543c292fa2225d63415ecd3e01e724e4c1
Demi Taylor <80684537+DigiDemii@users.noreply.github.com>
2022-02-01 10:44:27 +0000

    Initial Commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, Blueprint, render_template, url_for, redirect
+from model import db
+import random
+
+views = Blueprint(__name__, "views")
+
+app = Flask(__name__)
+
+app.register_blueprint(views, url_prefix="/")
+
+
+@app.route("/")
+def homepage():
+    card = random.choice(db)
+    return render_template("index.html", card=card)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
